4439d3b633d5930b6933eb2198f57b6d70413559
CameronByer <chunecake@gmail.com>
2022-07-08 10:11:21 -0400

    SudokuGrid Placeholder

--- App.js
+++ App.js
@@ -5,10 +5,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SudokuGrid style="auto" />
     </View>
   );
+}
+
+class SudokuGrid extends Component {
+  render() {
+    return (
+      <View style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+        <Text>Hello, world!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
